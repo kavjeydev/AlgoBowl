470497fba51517446c77e6ecfc88b9b8c1cb0332
unknown <kjey11205@gmail.com>
2024-02-20 17:34:16 -0800

    read input file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello")
+
+file = open("test.txt", "r")
+file_content = file.read().split("\n")
+
+num_courses_required = int(file_content[0]) # first like of input file is # courses required
+
+prerequisites_to_class = {}
+
+# key is classID, values are prerequisite IDs, dictionary holds this information
+for i in range(1, num_courses_required + 1):
+    prerequisites_to_class[i] = (file_content[i].split(" ")[1:])
+
+
+print(prerequisites_to_class)
