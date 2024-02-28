019593fbfc99799672387c2b90289272ffbea025
Aiden Jenkins <63414311+atxaidensworld@users.noreply.github.com>
2024-02-28 13:37:43 -0700

    Add files via upload

--- createGraph.py
+++ createGraph.py
@@ -1,6 +1,7 @@
 import networkx as nx
+import fileinput
 
-def createGraph():
+def createGraphText():
     name = input("Enter Name for Graph: ")
     print("Input file:")
     V = int(input())
@@ -17,5 +18,32 @@ def createGraph():
     G = nx.nx_pydot.to_pydot(Graph)
     G.write_png(f"{name}.png")
     return Graph
+
+def createGraphFile():
+    name = input("Enter Name of File to Read From w/o Filetype: ")
+    file = fileinput.input(f"{name}.txt")
+    V = int(file[0])
+    Graph = nx.DiGraph()
+    for i in range(V):
+        Graph.add_node((i+1))
+    for i in range(V):
+        line = file[i+1].split()
+        numE = int(line[0])
+        for j in range(numE):
+            Graph.add_edge(i+1,line[j+1])
+
+    G = nx.nx_pydot.to_pydot(Graph)
+    G.write_png(f"{name}.png")
+    return Graph
+
+def createGraph():
+    choice = input("Text(0) or File(1): ")
+    if choice == 0:
+        return createGraphText()
+    elif choice == 1:
+        return createGraphFile()
+    else:
+        print("Invalid Choice, Try Again!")
+        return createGraph()
 
 createGraph()
